ed0915e8274b62b9bcef69ed2c211929d9a48c67
Daria <daryasilantieva@gmail.com>
2019-12-17 16:04:09 +0300

    add login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { RegistrationComponent } from "./registration/registration.component";
+
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "login" },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegistrationComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/auth.ts
@@ -0,0 +1,10 @@
+export interface Auth {
+  id: number;
+  name: string;
+  email: string;
+
+  avatar: string;
+  is_admin: null;
+  created_at: string;
+  updated_at: string;
+}
