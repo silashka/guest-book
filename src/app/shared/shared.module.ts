124a7c1240e9461818b84bb5e952311c725f2635
Daria <daryasilantieva@gmail.com>
2020-01-03 18:03:31 +0300

    validation login

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
+
 @NgModule({
   imports: [FormsModule, ReactiveFormsModule],
   exports: [FormsModule, ReactiveFormsModule]
