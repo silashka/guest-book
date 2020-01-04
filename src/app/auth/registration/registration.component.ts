9e1f247580b70e5eee76fa5d6f0bee42fff8f43e
Daria <daryasilantieva@gmail.com>
2020-01-04 14:05:57 +0300

    start val register

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  selector: "app-registration",
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.css"]
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      avatar: new FormControl([Validators.maxLength(10000)]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(255)
+      ]),
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(255)
+      ]),
+      password: new FormControl([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(255)
+      ])
+    });
   }
-
+  onSubmit() {
+    console.log(this.form);
+  }
 }
