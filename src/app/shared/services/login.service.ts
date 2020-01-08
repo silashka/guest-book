5be67eb07d201137ddcd2d8835b326e80d33e7ee
Daria <daryasilantieva@gmail.com>
2020-01-08 11:26:49 +0300

    fix

--- src/app/shared/services/login.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "selenium-webdriver/http";
-
-@Injectable()
-export class UserService {
-  constructor(private http: HttpClient) {}
-}
